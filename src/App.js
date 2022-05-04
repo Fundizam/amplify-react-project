a453a2ff9dd2156f03aff608ead9727146bcb3cc
Luka <lukafunda1@gmail.com>
2022-05-04 20:00:10 +0200

    add storage WIP

--- src/App.js
+++ src/App.js
@@ -1,10 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect} from "react";
+import { Amplify, API, graphqlOperation, Storage } from 'aws-amplify';
+import { createTodo } from "./graphql/mutations";
+import { listTodos } from './graphql/queries'
 import { Authenticator } from "@aws-amplify/ui-react";
+import awsExports from "./aws-exports";
 import "@aws-amplify/ui-react/styles.css";
-function App() {
+Amplify.configure(awsExports);
 
+const initialState = { name: "", description: ""};
 
+const App = () => {
+
+  const [formState, setFormState] = useState(initialState)
+  const [todos, setTodos] = useState([])
+
+  useEffect(() => {
+    fetchTodos()
+  }, [])
+
+  function setInput(key, value) {
+    setFormState({ ...formState, [key]: value })
+  }
+
+  async function fetchTodos() {
+    try {
+      const todoData = await API.graphql(graphqlOperation(listTodos))
+      const todos = todoData.data.listTodos.items;
+      await Promise.all(todos.map(async todo =>
+        {
+          if (todo.image) 
+          {
+            const image = await Storage.get(todo.image);
+            todo.image = image;
+          }
+          return todo;
+        }))
+
+      setTodos(todos)
+    } catch (err) { console.log('error fetching todos') }
+  }
+
+  async function addTodo() 
+  {
+    try 
+    {
+      if (!formState.name || !formState.description)
+      {
+        return;
+      } 
+      const todo = { ...formState }
+      await API.graphql(graphqlOperation(createTodo, {input: todo}))
+      if (formState.image) 
+      {
+        const image = await Storage.get(formState.image);
+        formState.image = image;
+      }
+      setTodos([...todos, todo])
+      setFormState(initialState)
+    } 
+    catch (err) 
+    {
+      console.log('error creating todo:', err)
+    }
+  }
+
+  async function onChange(e)
+  {
+    if (!e.target.files[0]) 
+    {
+      return;      
+    }
+    const file = e.target.files[0];
+    setFormState({...formState, image: file.name});
+    await Storage.put(file.name, file);
+    fetchTodos();
+  }
 
 
   return (
@@ -27,12 +97,16 @@ function App() {
           value={formState.description}
           placeholder="Description"
         />
+        <input type="file" 
+          onChange={onChange}
+          style={styles.input}/>
         <button style={styles.button} onClick={addTodo}>Create Todo</button>
         {
           todos.map((todo, index) => (
             <div key={todo.id ? todo.id : index} style={styles.todo}>
               <p style={styles.todoName}>{todo.name}</p>
               <p style={styles.todoDescription}>{todo.description}</p>
+
             </div>
           ))
         }
@@ -42,4 +116,18 @@ function App() {
   );
 }
 
+const styles = {
+  container: { width: 400, margin: '0 auto', display: 'flex', flexDirection: 'column', justifyContent: 'center', padding: 20 },
+  todo: {  marginBottom: 15 },
+  input: { border: 'none', backgroundColor: '#ddd', marginBottom: 10, padding: 8, fontSize: 18 },
+  todoName: { fontSize: 20, fontWeight: 'bold' },
+  todoDescription: { marginBottom: 0 },
+  button: { backgroundColor: 'black', color: 'white', outline: 'none', fontSize: 18, padding: '12px 0px' }
+}
+
 export default App;
+/*
+<button onClick={() => deleteNote(note)}>Delete Note</button>
+{
+  note.image && <img src={note.image} style={{width: 400}}/>
+}*/
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Amplify } from "aws-amplify";
 import awsExports from "./aws-exports";
-Amplify.config(awsExports);
+Amplify.configure(awsExports);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
