6599070064b766100173008dd9b3e6125e415dd9
Luka <lukafunda1@gmail.com>
2022-05-04 18:37:55 +0200

    added auth

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "amplifyreactproject5fc9f01f": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,17 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { Authenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
 function App() {
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Version 3</h1>
-        <p>Lorem ipsum dolor sit amet.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, quibusdam.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, placeat.</p>
-      </header>
-    </div>
+    <Authenticator>
+      {({signOut, user}) => (
+        <div style={styles.container}>
+        <h1>Hello {user.username}</h1>
+        <button style={styles.button} onClick={signOut}>Sign out</button>
+        <br />
+        <h2>Amplify Todos</h2>
+        <input
+          onChange={event => setInput('name', event.target.value)}
+          style={styles.input}
+          value={formState.name}
+          placeholder="Name"
+        />
+        <input
+          onChange={event => setInput('description', event.target.value)}
+          style={styles.input}
+          value={formState.description}
+          placeholder="Description"
+        />
+        <button style={styles.button} onClick={addTodo}>Create Todo</button>
+        {
+          todos.map((todo, index) => (
+            <div key={todo.id ? todo.id : index} style={styles.todo}>
+              <p style={styles.todoName}>{todo.name}</p>
+              <p style={styles.todoDescription}>{todo.description}</p>
+            </div>
+          ))
+        }
+      </div>
+    )}
+    </Authenticator>
   );
 }
 
